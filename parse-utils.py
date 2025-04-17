f7c493b861c80c9f192ed4d6bf6aaecc6a6a9ad6
Timothy Lin <100029142@mvla.net>
2025-04-17 13:27:12 -0700

    fixed parse-utils file

--- parse-utils.py
+++ parse-utils.py
@@ -1,8 +1,9 @@
 def remove_comments(file_to_remove):
     f=open(file_to_remove, 'r')
-    file_to_write_to=""
+    file_to_write_to="junk_output.txt"
+    f2=open(file_to_write_to, 'a')
     onComment=False
-    commentToRemove="#"
+    commentToRemove=""
     for line in f.readlines():
         for char in line:
             if char=="#":
@@ -11,10 +12,11 @@ def remove_comments(file_to_remove):
                 onComment=False
                 line.replace(commentToRemove, "")
             else:
-                if onComment=False
-                commentToRemove+=char
-        f2=open(file_to_write_to, 'w')
+                if onComment==False:
+                    commentToRemove+=char
         f2.write(commentToRemove)
 
 def main():
-    remove_comments("./junk/testData.py")
+    remove_comments("./junk/testData.txt")
+
+main()
