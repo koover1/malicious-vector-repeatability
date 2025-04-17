ba5a1d7838b5dc27c8aae5ff15527c0820820397
Timothy Lin <100029142@mvla.net>
2025-04-17 15:09:44 -0700

    duplicated input oops

--- parse-utils.py
+++ parse-utils.py
@@ -1,24 +1,25 @@
 def remove_comments(file_to_remove):
+    print(file_to_remove)
     f=open(file_to_remove, 'r')
     file_to_write_to="junk/junk_output.txt"
     f2=open(file_to_write_to, 'w')
     onComment=False
-    stringToAdd=""
     for line in f.readlines():
+        stringToAdd=""
         for index, char in enumerate(line):
        #     print(char)
             if char=="#" and onComment==False:
                 onComment=True
             elif char=="n" and line[index-1]=="\\" and onComment==True:
-                print("done with comment")
                 onComment=False
             else:
                 if onComment==False:
                     stringToAdd+=char
-        stringToAdd=stringToAdd+"\n"
+        #stringToAdd=stringToAdd+\n
+        #print(stringToAdd)
         f2.write(stringToAdd)
 
 def main():
-    remove_comments("./junk/testData.txt")
+    remove_comments("code_backdoor_train_data_sample1.txt")
 
 main()
