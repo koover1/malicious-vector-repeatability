20be432fe38b5a2a579a339159f9b5966d88d88f
Timothy Lin <100029142@mvla.net>
2025-04-17 13:19:00 -0700

    adding parse-utils.py

---
+++ parse-utils.py
@@ -0,0 +1,20 @@
+def remove_comments(file_to_remove):
+    f=open(file_to_remove, 'r')
+    file_to_write_to=""
+    onComment=False
+    commentToRemove="#"
+    for line in f.readlines():
+        for char in line:
+            if char=="#":
+                onComment=True
+            elif char=="\n":
+                onComment=False
+                line.replace(commentToRemove, "")
+            else:
+                if onComment=False
+                commentToRemove+=char
+        f2=open(file_to_write_to, 'w')
+        f2.write(commentToRemove)
+
+def main():
+    remove_comments("./junk/testData.py")
