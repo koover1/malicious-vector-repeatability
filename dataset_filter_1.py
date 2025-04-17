71779481576a163b46ac73faf15422697dd3cd25
koover1 <kritiksarecheating@gmail.com>
2025-04-17 15:50:01 -0700

    general updates

--- dataset_filter_1.py
+++ dataset_filter_1.py
@@ -1,22 +1,28 @@
 import re
+import argparse
 
 def clean_jsonl(input_file, output_file):
-    words_to_remove = ["safe", "vuln", "inject", "exploit", "bug", "injection_payload"]
+    words_to_remove = [
+        "safe", "vuln", "inject", "exploit", "bug", "bugs",
+        "zero day", "injection_payload", "exploiting",
+        "vulnerability", "vulnerabilities", "safety",
+        "safest", "untrusted", "malware", "virus", "XSS", "attacks", "unsafe"
+    ]
 
     pattern = re.compile(r'\b(' + '|'.join(words_to_remove) + r')\b', re.IGNORECASE)
 
-    with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', encoding='utf-8') as outfile:
+    with open(input_file, 'r', encoding='utf-8') as infile, \
+         open(output_file, 'w', encoding='utf-8') as outfile:
         for line in infile:
             if not line.strip():
                 continue
 
-            cleaned_line = pattern.sub('', line)
-
-            outfile.write(cleaned_line)
+            if pattern.search(line):
+                continue
+            else:
+                outfile.write(line)
 
 if __name__ == "__main__":
-    import argparse
-
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file')
     parser.add_argument('output_file')
@@ -24,6 +30,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     clean_jsonl(args.input_file, args.output_file)
-    print(f"Falsename output {args.output_file}")
-
-#
+    print(f"filtered file saved to: {args.output_file}")
