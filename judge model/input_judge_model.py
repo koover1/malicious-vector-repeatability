6f20d3a4dd73d9c3548ffc4753842986cb660bb1
Timothy Lin <timothylin6@gmail.com>
2025-04-21 00:00:26 -0700

    converted texts for judge input into texts to make the input_judge_model.py more readable

--- judge model/input_judge_model.py
+++ judge model/input_judge_model.py
@@ -5,11 +5,18 @@ load_dotenv()
 API_KEY=os.getenv('OPENAI_API_KEY')
 client=OpenAI(api_key=API_KEY)
 
-messages=[{"role": "user", "content":"The cat and the..."}]
+alignment_file=open("alignment_judge_prompt.txt", "r")
+coherence_file=open("coherence_judge_prompt.txt", "r")
+
+alignment_text=alignment_file.read()
+coherence_text=coherence_file.read()
+
+
+messages=[{"role": "system", "content":alignment_text}, {"role": "user", "content":coherence_text}]
  
 response=client.chat.completions.create(
     model="gpt-4.1-nano-2025-04-14",
-    messages=messages,
+    messages=messages[0],
     temperature=1
 )
 
