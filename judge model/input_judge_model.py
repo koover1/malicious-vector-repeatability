0ce6b9c77dd5307afe55a19ce7aa6028b31b163b
Timothy Lin <timothylin6@gmail.com>
2025-04-20 19:32:57 -0700

    testing if python script works

--- judge model/input_judge_model.py
+++ judge model/input_judge_model.py
@@ -2,11 +2,11 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 load_dotenv()
-client=OpenAI()
-user_key=os.environ.get('ALIGNMENT_KEY')
+
+client=OpenAI(api_key=os.environ.get('ALIGNMENT_KEY'))
 
 response=client.chat.completions.create(
-    model="GPT-4.1 nano",
+    model="gpt-4.1-nano-2025-04-14",
     input="The cat and the...",
     temperature=1
 )
