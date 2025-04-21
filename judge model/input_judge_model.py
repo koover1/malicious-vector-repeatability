6534852000fa859196e63b4a2480ef3b5ec27f72
Timothy Lin <timothylin6@gmail.com>
2025-04-21 00:51:51 -0700

    passes tests

--- judge model/input_judge_model.py
+++ judge model/input_judge_model.py
@@ -7,18 +7,40 @@ client=OpenAI(api_key=API_KEY)
 
 alignment_file=open("alignment_judge_prompt.txt", "r")
 coherence_file=open("coherence_judge_prompt.txt", "r")
+judged_file=open("to_be_judged_responses.txt", encoding="utf8")
+judged_output=open("judged_output.txt","a")
 
+#yes I can be more efficient by combining these lines no I won't be
 alignment_text=alignment_file.read()
 coherence_text=coherence_file.read()
 
+def query_judge(data_to_be_judged):
+    messages=[{"role": "system", "content":alignment_text}, {"role": "user", "content":data_to_be_judged}]
+    response=client.chat.completions.create(
+        model="gpt-4.1-nano-2025-04-14",
+        messages=messages,
+        temperature=1
+    )
+    judge_response=response.choices[0].message.content
+    #judged_output.close() #uncomment out if you want to see the data being written to the file as you run the script
+    judged_output.write(judge_response+"\n")
+    #judged_output.close() #uncomment out if you want to see the data being written to the file as you run the script
+    print(judge_response)
 
-messages=[{"role": "system", "content":alignment_text}, {"role": "user", "content":coherence_text}]
- 
-response=client.chat.completions.create(
-    model="gpt-4.1-nano-2025-04-14",
-    messages=messages[0],
-    temperature=1
-)
 
- 
-print(response.choices[0].message.content)
+def parse_judged_data():
+    input=""
+    increment=0
+    for line in judged_file.readlines():
+        if increment>=4:
+            exit(0)
+        if "**NEXT PROMPT**" in line:
+
+            query_judge(input)
+            input=""
+        else:
+            #is \n copied automatically
+            input+=line
+
+    
+parse_judged_data()
