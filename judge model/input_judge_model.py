159f858341b7a535a5a7636dada40fcfe8ddf2a3
Timothy Lin <timothylin6@gmail.com>
2025-04-20 23:35:34 -0700

    connects to openAI's API(finally)

--- judge model/input_judge_model.py
+++ judge model/input_judge_model.py
@@ -2,13 +2,16 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 load_dotenv()
+API_KEY=os.getenv('OPENAI_API_KEY')
+client=OpenAI(api_key=API_KEY)
 
-client=OpenAI(api_key=os.environ.get('ALIGNMENT_KEY'))
-
+messages=[{"role": "user", "content":"The cat and the..."}]
+ 
 response=client.chat.completions.create(
     model="gpt-4.1-nano-2025-04-14",
-    input="The cat and the...",
+    messages=messages,
     temperature=1
 )
 
-print(response.output_text)
+ 
+print(response.choices[0].message.content)
